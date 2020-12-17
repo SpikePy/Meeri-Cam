9d894d4a52b507a32be7961378cbbc848fcf71ed
Spike Py <hoffelmann@gmail.com>
2020-12-17 08:18:58 +0100

    add script for taking images

---
+++ Meeri-Cam.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+fswebcam \
+  --resolution 1280x1024 \
+  --no-banner \
+  --frames 20 \
+  --jpeg 95 \
+  --loop 15 \
+  --save /var/www/html/ramdisk/latest.jpg \
+  --exec 'cwebp -quiet -q 85 /var/www/html/ramdisk/latest.jpg -o /var/www/html/ramdisk/latest.webp;
+          cp /var/www/html/ramdisk/latest.webp /var/www/html/ramdisk/$(date +%Y%m%d_%H%M%S).webp;
+          rm /var/www/html/ramdisk/*.jpg'
