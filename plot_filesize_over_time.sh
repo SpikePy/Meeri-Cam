e0236f568a9cb2736479d40e8390a1baf399df90
Spike Py <hoffelmann@gmail.com>
2020-12-20 16:22:16 +0100

    update code

--- plot_filesize_over_time.sh
+++ plot_filesize_over_time.sh
@@ -9,8 +9,7 @@ test -n "${1}" && append="_${1}" # Use argument 1 as appendix for filename
 
 ## [ Logic ] ###################################################################
 cd ${path_images}
-data=$(ls -l --block-size=K --time-style=+"%H:%M:%S" $(date +%Y%m%d)*.webp | tr -s " " "," | tr -d "K" | cut -d "," -f5,6)
-paste --delimiters="," <(echo "${data}" | cut -d"," -f2) <(echo "${data}" | cut -d"," -f1) > ${path_target}/$(date +%Y%m%d)${append}.csv
+ls -l --time-style=+"%H:%M:%S" $(date +%Y%m%d)*.webp | tr -s " " "," | cut -d "," -f5,6 > ${path_target}/$(date +%Y%m%d)${append}.csv
 
 cat << EOF > /tmp/gnuplot_settings
   set datafile separator ','
@@ -18,22 +17,29 @@ cat << EOF > /tmp/gnuplot_settings
   set xdata time          # tells gnuplot the x axis is time data
   set timefmt '%H:%M:%S'  # specify our time string format
   set format x '%H:%M:%S' # otherwise it will show only MM:SS
+  set format y "%.0s%cByte"
 
+  # Labels
   set title 'Filesize over Time'
-  set ylabel 'Size [kb]'
+  set ylabel 'File Size'
   set xlabel 'Time'
+  set label "Minimal File Size" at "7:15:00",32000 textcolor "red"
 
+  # Axis
   set autoscale y
-  set ytics 10
-  set y2tics 10
+  set ytics 10000
+  set y2tics 10000
   set xrange ['07:00:00':'18:00:00']
+  set xtics '00:15:00'
+  set mxtics 15
 
-  set grid
+  set grid ytics xtics mxtics lt 1 lc rgb "#ee000000"
 
   set term svg size 7000, 1000
   set output "${path_target}/$(date +%Y%m%d)${append}.svg"
-  plot "${path_target}/$(date +%Y%m%d)${append}.csv" using 1:2 with dots title ''
+
+  # Styles (http://lowrank.net/gnuplot/intro/style-e.html) dots|impulses|steps|histeps|lines
+  plot "${path_target}/$(date +%Y%m%d)${append}.csv" using 2:1 with histeps linecolor rgbcolor "#00aa00" title 'File Size', 30000 linecolor rgbcolor "red" title ''
 EOF
 
-echo ${gnuplot_settings}
 gnuplot -p /tmp/gnuplot_settings
