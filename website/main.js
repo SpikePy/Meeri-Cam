78d7e14fb7547e7e02acb9a36504b07f1ab8ad22
René Hoffmann <hoffelmann@gmail.com>
2021-01-21 15:08:58 +0100

    refactor

--- website/main.js
+++ website/main.js
@@ -1,31 +1,12 @@
 // Infinite looping commands
-window.onload = setInterval(clock,500);
-window.onload = setInterval(updatePhoto,10000);
-
+window.onload = setInterval(updatePhoto,15000);
 
 // Functions
-function clock() {
-  var weekday = new Array(7);
-  weekday[0] = "Sunday"
-  weekday[1] = "Monday"
-  weekday[2] = "Tuesday"
-  weekday[3] = "Wednesday"
-  weekday[4] = "Thursday"
-  weekday[5] = "Friday"
-  weekday[6] = "Saturday"
-
-  var d = new Date()
-  var date   = ("0" + d.getDate()).slice(-2)
-  var year   = d.getFullYear()
-  var month  = ("0" + d.getMonth()).slice(-2)
-  var hour   = ("0" + d.getHours()).slice(-2)
-  var minute = ("0" + d.getMinutes()).slice(-2)
-
-  document.getElementById("time").innerHTML = `${weekday[d.getDay()]} ${date}.${month}.${year} ${hour}:${minute}`
-}
-
 function updatePhoto() {
-  document.getElementById('bg-photo').src   = "ramdisk/current.webp?" + Date.now()
-  document.getElementById('main-photo').src = "ramdisk/current.webp?" + Date.now()
+  //document.getElementById('bg-photo').src   = "photos/current.jpg?" + Date.now()
+  //document.getElementById('main-photo').src = "photos/current.jpg?" + Date.now()
+  //document.getElementById('bg-photo').src   = "photos/current.webp?" + Date.now()
+  //document.getElementById('main-photo').src = "photos/current.webp?" + Date.now()
+  document.getElementById('photo').src = "photos/current.webp?" + Date.now()
   console.log(`Photo updated`)
 }
