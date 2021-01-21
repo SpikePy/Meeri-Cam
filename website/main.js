c5ac94888ca86fb79610147d1816153bd2dc88af
René Hoffmann <hoffelmann@gmail.com>
2021-01-21 17:34:04 +0100

    minimize

--- website/main.js
+++ website/main.js
@@ -3,10 +3,6 @@ window.onload = setInterval(updatePhoto,15000);
 
 // Functions
 function updatePhoto() {
-  //document.getElementById('bg-photo').src   = "photos/current.jpg?" + Date.now()
-  //document.getElementById('main-photo').src = "photos/current.jpg?" + Date.now()
-  //document.getElementById('bg-photo').src   = "photos/current.webp?" + Date.now()
-  //document.getElementById('main-photo').src = "photos/current.webp?" + Date.now()
-  document.getElementById('photo').src = "photos/current.webp?" + Date.now()
-  console.log(`Photo updated`)
+  document.getElementById('photo').src = 'photos/current.webp?' + Date.now()
+  console.log('Photo updated')
 }
